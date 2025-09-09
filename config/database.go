787e7fb60ae921e962688d314d045c96dab570a4
gallanpw <gallan.widyanto@gmail.com>
2025-09-09 19:51:32 +0700

    Fix: Update database.go

--- config/database.go
+++ config/database.go
@@ -1,22 +1,23 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
 
-	// "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-func ConnectDB() error {
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatalf("Error loading .env file")
-	// 	// log.Println("Error loading .env file. Assuming environment variables are set in Railway.")
-	// }
+func ConnectDB() {
+	err := godotenv.Load()
+	if err != nil {
+		// log.Fatalf("Error loading .env file")
+		log.Println("Error loading .env file. Assuming environment variables are set in Railway.")
+	}
 
 	// dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require TimeZone=Asia/Jakarta",
 	// 	os.Getenv("DB_HOST"),
@@ -30,20 +31,20 @@ func ConnectDB() error {
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
 		// Jika tidak ada, gunakan variabel lingkungan manual (untuk kompatibilitas)
-		// dsn = fmt.Sprintf(
-		// 	"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=Asia/Jakarta",
-		// 	os.Getenv("DB_HOST"),
-		// 	os.Getenv("DB_PORT"),
-		// 	os.Getenv("DB_USER"),
-		// 	os.Getenv("DB_PASSWORD"),
-		// 	os.Getenv("DB_NAME"),
-		// 	os.Getenv("DB_SSLMODE"),
-		// )
-		log.Fatal("DATABASE_URL not found. Please set it in your Railway variables.")
+		dsn = fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=Asia/Jakarta",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_SSLMODE"),
+		)
+		// log.Fatal("DATABASE_URL not found. Please set it in your Railway variables.")
 		// log.Println("DATABASE_URL not found. Please set it in your Railway variables.")
 	}
 
-	var err error
+	// var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		// panic(err)
@@ -52,7 +53,7 @@ func ConnectDB() error {
 	}
 
 	// log.Fatal("Database connection successfully opened.")
-	// log.Println("Database connection successfully opened.")
+	log.Println("Database connection successfully opened.")
 	// fmt.Println("Database connection successfully opened.")
-	return nil
+	// return nil
 }
